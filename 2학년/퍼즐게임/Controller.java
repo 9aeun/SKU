e40668ae822ad3878ee3e347bf8d9622aea7920a
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-01-31 21:36:24 +0900

    Create Controller.java

---
+++ 2학년/퍼즐게임/Controller.java
@@ -0,0 +1,19 @@
+package Final;
+
+public class Controller {
+	Model model = new Model();
+	View view = new View();
+
+	public void gameStart(){
+		view.setSize(1000,700);
+		view.setVisible(true);
+		
+		Sound test = new Sound();
+		try {
+			test.Music();
+			Thread.sleep(213000);
+		} catch (InterruptedException e) {				
+			e.printStackTrace();
+		}
+	}
+}
