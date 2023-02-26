25406bea96d104078b6b9b4e0fa9c8195e70a8df
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-02-26 19:28:57 +0900

    Update BoardController.java

--- 3학년/JSP/mvc/BoardController.java
+++ 3학년/JSP/mvc/BoardController.java
@@ -17,7 +17,7 @@ public class BoardController extends HttpServlet {//게시판을 control 하는 
    private static final long serialVersionUID = 1L;
    static final int LISTCOUNT = 5; 
 
-   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {//ㅎㅏ기싫다
       doPost(request, response);
    }
 
