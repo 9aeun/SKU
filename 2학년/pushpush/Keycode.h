58ca708404a32357dccee311276471c746f690c7
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-07-08 16:41:55 +0900

    Create Keycode.h

---
+++ 2학년/pushpush/Keycode.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<iostream>
+using namespace std;
+#define UPKEY 0xe048
+#define LEFTKEY 0xe04b
+#define RIGHTKEY 0xe04d
+#define DOWNKEY 0xe050
+#define ESC 27
+#define SPACE 32
